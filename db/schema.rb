21ffe890dc744ecb0fc7d80e0809acf1eacdd02a
Vlad <vlad@hotmail.co.nz>
2017-08-23 16:29:52 -0700

    finished models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823222020) do
+ActiveRecord::Schema.define(version: 20170823225715) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
