a637474bc8a46b27a344ebdd3212d813dd65a7e2
Vlad <vlad@hotmail.co.nz>
2017-08-23 17:24:12 -0700

    finished validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
+	validates :body, presence: true, length: { maximum: 255 }
+	validates :post, presence: true
+	validates :user, presence: true
 	belongs_to :user
 	belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
-
+	validates :title, presence: true, length: { minimum: 6}
+	validates :body, presence: true, length: { maximum: 1000 }
+	validates :user, presence: true
 	belongs_to :user
 	has_many :comments
 end
