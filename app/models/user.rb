2235e09537122a9fbee1b887b247c2cd6b83c166
Vlad <vlad@hotmail.co.nz>
2017-08-23 17:00:54 -0700

    added more validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255},
 	format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-
+	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+	has_secure_password
 	has_many :posts
+	has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823225715) do
+ActiveRecord::Schema.define(version: 20170823233611) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170823225715) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
   end
