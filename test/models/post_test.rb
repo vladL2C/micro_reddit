a9c0556e9cfa5329342a86b4c3bdbea0e69c6bb9
Vlad <vlad@hotmail.co.nz>
2017-08-23 21:16:30 -0700

    finished all tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -7,5 +7,27 @@ class PostTest < ActiveSupport::TestCase
 			user_id: 1)
 	end 
 
-	
+	test "title is not empty" do 
+		@post.title = " "
+		@post.save 
+		assert_not @post.valid?
+	end 
+
+	test "body is not empty" do 
+		@post.body = " "
+		@post.save 
+		assert_not @post.valid?
+	end 
+
+	test "title is minimum of 6 chars" do 
+		@post.title = "a" * 5 
+		@post.save 
+		assert_not @post.valid?
+	end 
+
+	test "body is maximum of 1000 chars" do 
+		@post.body = "a" * 1001
+		@post.save
+		assert_not @post.valid?
+	end 
 end
